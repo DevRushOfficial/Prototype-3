0c038db2adaa9eb1d54c07440f005df11811c025
Artem Melnykov <artemmelnykov29@gmail.com>
2024-08-12 21:30:16 +0200

    Added gravity and changed its value

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody _playerRb;
     private float _jumpForce = 10;
+    private float _gravityModifier;
 
     void Start()
     {
         _playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= _gravityModifier;
     }
 
     void Update()
