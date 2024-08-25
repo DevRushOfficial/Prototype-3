22a384d733239c615f8f8198caab7fef8cc1e2d8
Artem Melnykov <artemmelnykov29@gmail.com>
2024-08-25 18:40:12 +0200

    Added obstacle spawning with 2s interval, cleaned up code

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject spawnPrefab;
+    public GameObject obstaclePrefab;
     private Vector3 _spawnPosition = new Vector3(15, 0, 0);
+    private float _startDelay = 2;
+    private float _repeatRate = 2;
 
     void Start()
     {
-        Instantiate(spawnPrefab, _spawnPosition, spawnPrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacles", _startDelay, _repeatRate);
     }
 
-    void Update()
+    void SpawnObstacles()
     {
-        
+        Instantiate(obstaclePrefab, _spawnPosition, obstaclePrefab.transform.rotation);
     }
 }
