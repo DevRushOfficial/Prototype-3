fec2efe86a148c5d5d0ebc932bfbe50091425110
Artem Melnykov <artemmelnykov29@gmail.com>
2024-08-29 18:31:08 +0200

    Add background repeat functionality

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 _startPosition;
+    private float _repeatWidth = 56.4f;
+    void Start() => _startPosition = transform.position;
 
-    // Update is called once per frame
-    void Update()
+    void Update() => ResetPosition();
+
+    private void ResetPosition()
     {
-        
+        if (transform.position.x < _startPosition.x - _repeatWidth)
+        {
+            transform.position = _startPosition;
+        }
     }
 }
